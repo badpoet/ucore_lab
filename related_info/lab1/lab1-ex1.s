5226bb540897f63c666fad69242af3a3ba855be9
Xunkai Zhang <xunkai55@gmail.com>
2016-03-13 21:57:33 +0800

    lab1 related lab

---
+++ related_info/lab1/lab1-ex1.s
@@ -0,0 +1,13 @@
+
+lab1-ex1.o：     文件格式 elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <main>:
+   0:	55                   	push   %rbp
+   1:	48 89 e5             	mov    %rsp,%rbp
+   4:	bf 00 00 00 00       	mov    $0x0,%edi
+   9:	e8 00 00 00 00       	callq  e <main+0xe>
+   e:	5d                   	pop    %rbp
+   f:	c3                   	retq   
